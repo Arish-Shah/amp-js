f4d60bc8240ddfc9810785a639e43679ac84c265
Arish-Shah <arish0246@gmail.com>
2020-03-15 19:38:18 +0530

    adds demo

---
+++ demo/app.js
@@ -0,0 +1,25 @@
+import { html, render } from '../dist/amp.js';
+
+const root = document.getElementById('root');
+let data = {
+  message: 'World',
+  count: 0
+};
+
+const handleClick = () => {
+  data.count++;
+  render(Template(data), root);
+};
+
+const handleInput = event => {
+  data.message = event.target.value;
+  render(Template(data), root);
+};
+
+const Template = data => html`
+  <input type="text" .value=${data.message} @input=${handleInput} />
+  <button @click=${handleClick}>Clicks: ${data.count}</button>
+  <h1>Hello ${data.message || 'Stranger'}!</h1>
+`;
+
+render(Template(data), root);
